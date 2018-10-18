d99f3de3d9bba2801fe7b571e264535fc982c20f
lukealba <lalbarracin2000@gmail.com>
2018-10-18 11:42:26 -0500

    Still making some changes. Can take parameters now...

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
 use std::any::Any;
+use std::io::Write;
 use std::fmt::Debug;
 
-/*  macro_rules! trampoline {
-    ($x:expr) => (recur($x)) // Get unknown parameters and do more ...
-} */
+//macro_rules! trampoline {
+//   ($x:expr) => (println!("This is my project"))
+//}
 
+macro_rules! tail_recur {
+    (fn $name:ident $params:tt $body:tt) => (recur($params, $name()))
+}
+
+// need to change unsized function (dynamically sized function) like &[T] -- use destructuring???
 fn recur <T: 'static + Debug> (_params : T, _func : fn(T) -> T) -> T where T: Copy + PartialEq + ToString {
     let mut _memoize = _params;
         loop {
             let _temp = _func (_memoize);
             let foo = 8.to_string();
             println!("Debug: {:?}", &foo);
-            if Some(&_temp.clone().to_string()) == Some(&foo) {
-                println! ("Conditional complete !!!");
+            if Some(&_temp.to_string()) == Some(&foo) {
+                println!("Conditional complete !!!");
+                _memoize = _temp;
                 break;
             } else {
                 _memoize = _temp;
                 println!("Value of temp : {:?}", &_temp);
-                println! ("Value of Memoize : {:?}",  &_memoize);
+                println!("Value of Memoize : {:?}",  &_memoize);
             }
         }
     _memoize
@@ -28,5 +35,7 @@ fn add_two (_num : u32) -> u32 {
 }
 
 fn main() {
-   println!("{}", recur (0, add_two));
+   //trampoline!(0+0);
+   //tail_recur!(add_two(0));
+   //println!("{}", recur (0, add_two));
 }

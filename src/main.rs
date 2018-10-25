2864f9b06a352d6533e61067956540dfb8e5bee1
lukealba <lalbarracin2000@gmail.com>
2018-10-24 21:38:49 -0500

    Still making some changes

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,26 @@ macro_rules! tail_recur { //put a conditional within the macro
 }
 
 macro_rules! recur_fn {
-   //($x:item) => ($x);
-   (fn $fpointer:ident $fparams:tt $x:tt $ftr:ty $fbody:block) =>
-        (fn $fpointer $fparams $x $ftr  {
-               $fbody
+   //(fn $fpointer:ident $fparams:tt $arrow:tt $rtrn_type:ty $fbody:block) =>
+   //     (fn $fpointer $fparams $arrow $rtrn_type {
+   //            $fbody
+   //     });
+   //($fbody:block) => 
+   ($(($pname:ident $colon:tt $type:ty)),*) => ($($type),*);
+   ($fpointer:ident $fparams:tt $arrow:tt $rtrn_type:ty $fbody:block) =>
+        (fn $fpointer $fparams $arrow $rtrn_type {
+            let _memoize = $fparams;
+            'fn_loop : loop {
+                fn recur $fparams $arrow recur_fn!($fparams) {
+
+                }
+                //if foo == true else ...
+            }
         });
+   //($(fn $fpointer $gens:ty $fparams:tt $arrow:tt $rtrn_type:ty $fbody:block)) =>
+   //     $();
+    //_ => (panic!("Incorrectly formated function..."))
 }
-
-
 
 // need to change unsized function (dynamically sized function) like &[T] -- use destructuring???
 fn recur <T: 'static + Debug> (_params : T, _func : fn(T) -> T) -> T where T: Copy + PartialEq + ToString {
@@ -35,18 +47,16 @@ fn recur <T: 'static + Debug> (_params : T, _func : fn(T) -> T) -> T where T: Co
 }
 
 recur_fn!{
-fn add_two (_num : u32) -> u32 {
-    _num + 2
-}}
+    add_two (_num : u32) -> u32 {
+        _num + 2
+    }
+}
 
+#[warn(dead_code)]
 fn add_together (_num1 : u32, _num2 : u32) -> u32 {
     _num1 + _num2
 }
 
 fn main() {
-   //trampoline!(0+0);
-   //println!("{:?}", tail_recur!(add_two(0)));
-   println!("{:?}", add_two(5));
-   //tail_recur!(add_together(1,3));
-   //println!("{}", recur (0, add_two));
+   //println!("{:?}", add_two(5));
 }

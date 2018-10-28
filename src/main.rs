c312afd713bd1c803f57f0781bb150692b8cb5fa
lukealba <lalbarracin2000@gmail.com>
2018-10-28 00:13:49 -0500

    Fixing scope error...

--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,42 @@ use std::fmt::Debug;
 use std::ops::BitOr;
 
 macro_rules! recur {
-    ($($expr:expr),*) => ({return ($($expr),*)})
+    ($($pexpr:expr),*) => {_memoize = ($($pexpr),*)}
 }
 
 macro_rules! recur_fn {
-    ($fpointer:ident $fparams:tt $arrow:tt $rtrn_type:ty $fbody:block) => 
-        (fn $fpointer $fparams $arrow $rtrn_type {
-            let _memoize = recur_fn!(destructure_args $fparams);
+    ($fpointer:ident ($($pname:ident $colon:tt $type:ty),*) $arrow:tt $rtrn_type:ty $fbody:block) => 
+        (fn $fpointer ($($pname $colon $type),*) $arrow $rtrn_type {
+            let _memoize = ($($pname),*);
             'fn_loop : loop {
                 // can rloop have 0 arguments (impure function)???
-                fn rloop $fparams $arrow recur_fn!(destructure_types $fparams) {
+                fn rloop ($($pname $colon $type),*) $arrow ($($type),*) {
                     //multiple closures that take fbody (e.g. |x| = x + 1) POSSIBLY NOT MANDATORY???
                       let foo = Box::new(|x| $fbody); //should return a tuple of the updated arguments to be looped over...
-                      let baz = foo(1);
+                      let baz = foo(0);
+                      return baz
                       (0,0)
                 }
                 break 'fn_loop;
                 _memoize = rloop(0, 1);
             }
-            fn get_final_val $fparams $arrow $rtrn_type {
-                $fbody
-            }
-            get_final_val (0, 2)
+            fn get_final_val ($($pname $colon $type),*) $arrow $rtrn_type $fbody
+            get_final_val (1, 2)
         });
-    //(fn $fpointer $fparams $arrow $rtrn_type {$fbody});
-    (destructure_args ($($pname:ident $colon:tt $type:ty),*)) => (($($pname),*));
-    (destructure_types ($($pname:ident $colon:tt $type:ty),*)) => (($($type),*))
+    //(destructure_args ($($pname:ident $colon:tt $type:ty),*)) => (($($pname),*));
+    //(destructure_types ($($pname:ident $colon:tt $type:ty),*)) => (($($type),*))
    //($(fn $fpointer $gens:ty $fparams:tt $arrow:tt $rtrn_type:ty $fbody:block)) => $();
 }
 
  recur_fn!{
     add_together (_num1 : u32, _num2 : u32) -> u32 {
-    _num1 + _num2
+       let sum = _num1 + _num2;
+       if sum < 10 {
+           recur!(_num1 + 1, _num2 + 1);
+           return sum
+       } else {
+            150
+       }
     }   
 }
 

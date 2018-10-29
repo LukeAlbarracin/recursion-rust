a9ab8f0dc650cbc7c55150a8e3cad260993f45d5
lukealba <lalbarracin2000@gmail.com>
2018-10-29 18:36:49 -0500

    Still fixing some issues...

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,29 @@
 #[allow(unused_imports)]
 use std::fmt::Debug;
 use std::ops::BitOr;
+use std::mem;
+
 
 macro_rules! recur {
-    ($($pexpr:expr),*) => {_memoize = ($($pexpr),*)}
+    //($($pexpr:expr),*) => {{let z = ($($pexpr),*); z}}
+    ($($pexpr:expr),*) => {($($pexpr),*)}
+    // remember that it is either return / changing outside environment
 }
 
 macro_rules! recur_fn {
     ($fpointer:ident ($($pname:ident $colon:tt $type:ty),*) $arrow:tt $rtrn_type:ty $fbody:block) => 
-        (fn $fpointer ($($pname $colon $type),*) $arrow $rtrn_type {
-            let _memoize = ($($pname),*);
-            'fn_loop : loop {
-                // can rloop have 0 arguments (impure function)???
-                fn rloop ($($pname $colon $type),*) $arrow ($($type),*) {
-                    //multiple closures that take fbody (e.g. |x| = x + 1) POSSIBLY NOT MANDATORY???
-                      let foo = Box::new(|x| $fbody); //should return a tuple of the updated arguments to be looped over...
-                      let baz = foo(0);
-                      return baz
-                      (0,0)
-                }
-                break 'fn_loop;
-                _memoize = rloop(0, 1);
-            }
-            fn get_final_val ($($pname $colon $type),*) $arrow $rtrn_type $fbody
-            get_final_val (1, 2)
-        });
-    //(destructure_args ($($pname:ident $colon:tt $type:ty),*)) => (($($pname),*));
-    //(destructure_types ($($pname:ident $colon:tt $type:ty),*)) => (($($type),*))
+        (fn $fpointer ($($pname : $type),*) -> $rtrn_type {
+            let mut _memoize = ($($pname),*);
+            let mut done = false;
+            let mut outer_closure = move |x| while !done { // should be changed to a while loop...
+                done = true;
+                let mut inner_closure = Box::new(|y| $fbody);
+                inner_closure(_memoize);
+            };
+            outer_closure(_memoize);
+            fn get_final_val ($($pname : $type),*) -> $rtrn_type $fbody
+            get_final_val (5, 5)
+        })
    //($(fn $fpointer $gens:ty $fparams:tt $arrow:tt $rtrn_type:ty $fbody:block)) => $();
 }
 
@@ -34,8 +31,7 @@ macro_rules! recur_fn {
     add_together (_num1 : u32, _num2 : u32) -> u32 {
        let sum = _num1 + _num2;
        if sum < 10 {
-           recur!(_num1 + 1, _num2 + 1);
-           return sum
+           recur!(_num1 + 1, _num2 + 1)
        } else {
             150
        }
@@ -48,5 +44,5 @@ fn add_two (_num : u32) -> u32 {
 
 fn main() {
    println!("Tail recursive macro almost done!!!");
-   println!("{:?}", add_together(1, 4));
+   println!("{:?}", add_together(5, 5));
 }

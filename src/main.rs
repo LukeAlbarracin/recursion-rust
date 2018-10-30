9301e424d13a8e2e4f03879a0a049dc0f32b2121
lukealba <lalbarracin2000@gmail.com>
2018-10-29 20:44:05 -0500

    Nearly finished this masterpiece...

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 #[allow(unused_imports)]
 use std::fmt::Debug;
+#[allow(unused_imports)]
 use std::ops::BitOr;
+#[allow(unused_imports)]
 use std::mem;
 
-
-macro_rules! recur {
+#[allow(unused_macros)]
+macro_rules! recurse {
     //($($pexpr:expr),*) => {{let z = ($($pexpr),*); z}}
-    ($($pexpr:expr),*) => {($($pexpr),*)}
-    // remember that it is either return / changing outside environment
+    ($($pexpr:expr),*) => ({binding = ($($pexpr),*); 0})
 }
 
 macro_rules! recur_fn {
@@ -15,14 +16,16 @@ macro_rules! recur_fn {
         (fn $fpointer ($($pname : $type),*) -> $rtrn_type {
             let mut _memoize = ($($pname),*);
             let mut done = false;
-            let mut outer_closure = move |x| while !done { // should be changed to a while loop...
+            let mut outer_closure = move || while !done { // should be changed to a while loop...
                 done = true;
-                let mut inner_closure = Box::new(|y| $fbody);
+                let recur = move |x| { _memoize = x; done = false;0};
+                let inner_closure = Box::new(move |y| $fbody);
                 inner_closure(_memoize);
             };
-            outer_closure(_memoize);
+            outer_closure();
+            fn recur ($($pname : $type),*) -> $rtrn_type {$fbody}
             fn get_final_val ($($pname : $type),*) -> $rtrn_type $fbody
-            get_final_val (5, 5)
+            get_final_val (3, 3)
         })
    //($(fn $fpointer $gens:ty $fparams:tt $arrow:tt $rtrn_type:ty $fbody:block)) => $();
 }
@@ -31,9 +34,10 @@ macro_rules! recur_fn {
     add_together (_num1 : u32, _num2 : u32) -> u32 {
        let sum = _num1 + _num2;
        if sum < 10 {
-           recur!(_num1 + 1, _num2 + 1)
+           println!("Okay");
+           recur(_num1 + 1, _num2 + 1)
        } else {
-            150
+            sum
        }
     }   
 }
@@ -44,5 +48,5 @@ fn add_two (_num : u32) -> u32 {
 
 fn main() {
    println!("Tail recursive macro almost done!!!");
-   println!("{:?}", add_together(5, 5));
+   println!("{:?}", add_together(3, 3));
 }

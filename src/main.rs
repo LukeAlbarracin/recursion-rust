722a6a32b7f6320a059e36cc51a6a21dfc5b1137
lukealba <lalbarracin2000@gmail.com>
2018-10-30 19:04:49 -0500

    Polished the code for conciseness...

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,21 @@
 #![allow(unused_variables)]
+#![recursion_limit="1000"]
 macro_rules! recur_fn {
     ($fpointer:ident ($($pname:ident : $type:ty),*) -> $rtrn_type:ty $fbody:block) => 
         (fn $fpointer ($($pname : $type),*) -> $rtrn_type {
             let mut _memoize = ($($pname),*);
-            let mut done = false;
-            let outer_closure = move || while !done {
-                done = true;
-                let recur = move |x|  {_memoize = x; done = false;};
-                let inner_closure = Box::new(move |y| $fbody);
-                inner_closure(_memoize);
-            };
             fn recur ($($pname : $type),*) -> $rtrn_type $fbody
             let tail_val = move |_tailp| $fbody;
             tail_val (_memoize)
         })
-   //($(fn $fpointer $gens:ty $fparams:tt -> $rtrn_type:ty $fbody:block)) => $();
 }
 
  recur_fn!{
     add_together (_num1 : u32, _num2 : u32) -> u32 {
        let sum = _num1 + _num2;
-       if sum < 10 {
+       if sum < 5 {
            println!("{}", sum);
-           recur(_num1 + 1, _num2 + 1)
+           recur(recur(_num1 + 1, _num2 + 1), recur(_num1 + 1, _num2 + 1))
        } else {
             sum
        }

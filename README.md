ff5e53d813ba1090d6e59a02a7141e117757750a
lukealba <lalbarracin2000@gmail.com>
2018-11-17 10:45:00 -0600

    Edited major misconception

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
 # recursion-rust
 
-Extremely concise macro that utilizes the hygienic macros of Rust to allow tail calls
-
-Look at the below code :
-
-  macro_rules! recur_fn {($item:item)=>($item)}
-  
- 
-Now by wrapping my code within the macro :
-  
-  recur_fn! { 
-    fn fibonacci_recur (_num : u32) -> u32 {
-        if _num == 0 || _num == 1 {
-           _num
-       } else {
-          fibonacci_recur(_num - 1) + fibonacci_recur(_num - 2)
-       }
-    }
-  }
-
-Instead of getting a stack overflow error or an error that forbids a tail call, we are allowed to call the function
-recursively. This is because of the hygienic macro structure of rust. In order to avoid confusion amongst identifiers it often assigns divides the identifier based on it syntactical context to the function pointer, resulting in the macro wrapping itself like foobar(foobar(foobar())). Yes, this is not pure tail call optimization but it is the closest way to replicate a tail call without a stack overflow error. (See hygienic macros at : https://doc.rust-lang.org/book/first-edition/macros.html) 
-
+Still working on stuff...
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,34 @@
-#![allow(dead_code)]
 macro_rules! recur_fn {
-    (fn $fpointer:ident ($($pname:ident : $type:ty),*) -> $rtrn_type:ty $fbody:block) => 
+    ($fpointer:ident ($($pname:ident $colon:tt $type:ty),*) $arrow:tt $rtrn_type:ty $fbody:block) => 
         (fn $fpointer ($($pname : $type),*) -> $rtrn_type {
+            let mut _memoize = ($($pname),*);
+            let mut done = false;
+            let mut outer_closure = move || while !done {
+                done = true;
+                let recur = move |x|  {_memoize = x; done = false;};
+                let inner_closure = Box::new(move |y| $fbody);
+                inner_closure(_memoize);
+            };
             fn recur ($($pname : $type),*) -> $rtrn_type $fbody
             let tail_val = move |_tailp| $fbody;
-            tail_val ($($pname),*)
-        });
-    ($item:item) => ($item)
+            tail_val (_memoize)
+        })
+   //($(fn $fpointer $gens:ty $fparams:tt $arrow:tt $rtrn_type:ty $fbody:block)) => $();
 }
 
-recur_fn! {
-    fn fibonacci_recur (_num : u32) -> u32 {
-        if _num == 0 || _num == 1 {
-           _num
+ recur_fn!{
+    add_together (_num1 : u32, _num2 : u32) -> u32 {
+       let sum = _num1 + _num2;
+       if sum < 10 {
+           println!("Okay");
+           recur(_num1 + 1, _num2 + 1)
        } else {
-          fibonacci_recur(_num - 1) + fibonacci_recur(_num - 2)
+            sum
        }
-    }
-}
-
-recur_fn! {
-    fn recursive_iteration (_num : u32) -> u32 {
-        if _num < 4 {
-            recur (_num + 1)
-        } else {
-            _num
-        }
-    }
-}
-
-recur_fn! {
-    fn print_something() {
-        println!("This does absolutely nothing except print this...");
-    }
+    }   
 }
 
 fn main() {
-   println!("Hello, Tail Recursive Macro!");
-   assert_eq!(8, fibonacci_recur(6)); // Fibonacci sequence : 1,1,2,3,5,8,13,21...
-   assert_eq!(4, recursive_iteration(0));
-   print_something();    
+   println!("Tail recursive macro almost done!!!");
+   println!("{:?}", add_together(0, 0));
 }

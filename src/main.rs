1b69f1738bf9234b0114bd77797b6b1a262f01ad
lukealba <lalbarracin2000@gmail.com>
2018-11-28 22:41:29 -0600

    Fixing logic currently...

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,30 @@
 macro_rules! recur_fn {
-    (fn $fpointer:ident ($($pname:ident : $type:ty),*) -> $rtrn_type:ty $fbody:block) =>{
-        fn $fpointer ($($pname : $type),*) -> $rtrn_type  {
-            //fn recur ($($pname:$type),*)->($($type),*) {($($pname),*)}
-            let _memoize = ($($pname),*);
-            'outer : loop {
-                fn recur ($($pname:$type),*)->($($type),*) {($($pname),*)}
-                let foo = |x| {$fbody};
-                match foo(_memoize) {
-                    ($($pname),*) => {},
-                    _ => {break 'outer;}
-                }
-                _memoize = foo;              
+    ($fpointer:ident ($($pname:ident $colon:tt $type:ty),*) $arrow:tt $rtrn_type:ty $fbody:block) => 
+        (fn $fpointer ($($pname : $type),*) -> $rtrn_type {
+            let mut _memoize = (true,$($pname),*);
+            while _memoize.0 {
+                println!("Received!");
+                //fn recur (again:bool, $($pname: $type),*) -> (bool,($($type),*)) {(true,($($pname),*))}
+                fn recur (repeat:bool,$($pname:$type),*) -> (bool,$($type),*) {(true,$($pname),*)}
+                _memoize = {$fbody};
             }
-            42
-        }
-    }
+            fn arb_fn ($($pname : $type),*) -> $rtrn_type {$fbody}
+            println!("{:?}", arb_fn(_memoize.0));
+        })
+   //($(fn $fpointer $gens:ty $fparams:tt $arrow:tt $rtrn_type:ty $fbody:block)) => $();
 }
-        
-recur_fn!{
-    fn add_together (_num1 : u32, _num2 : u32) -> u32 {
+
+ recur_fn!{
+    add_together (_num1 : u32, _num2 : u32) -> u32 {
        let sum = _num1 + _num2;
        if sum < 10 {
-           println!("This test ran okay!!!");
-           recur(_num1+1, _num2+1)
+           println!("Okay");
+           recur(_num1 + 1, _num2 + 1)
        } else {
             sum
        }
     }   
 }
-
-
 
 fn main() {
    println!("Tail recursive macro almost done!!!");

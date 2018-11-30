8dd525791f81908f3a9c1ad09bb276e331148b55
Narciso Luke Albarracin <lalbarracin2000@gmail.com>
2018-11-30 10:22:33 -0600

    Fixed some small errors

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 macro_rules! recur_fn {
-    ($fpointer:ident ($($pname:ident $colon:tt $type:ty),*) $arrow:tt $rtrn_type:ty $fbody:block) => 
+    ($fpointer:ident ($($pname:ident : $type:ty),*) -> $rtrn_type:ty $fbody:block) => 
         (fn $fpointer ($($pname : $type),*) -> $rtrn_type {
             let mut _memoize = (true,$($pname),*);
             while _memoize.0 {
-                println!("Received!");
-                //fn recur (again:bool, $($pname: $type),*) -> (bool,($($type),*)) {(true,($($pname),*))}
+                println!("Test complete...");
                 fn recur (repeat:bool,$($pname:$type),*) -> (bool,$($type),*) {(true,$($pname),*)}
                 _memoize = {$fbody};
             }
             fn arb_fn ($($pname : $type),*) -> $rtrn_type {$fbody}
             println!("{:?}", arb_fn(_memoize.0));
-        })
+    });
    //($(fn $fpointer $gens:ty $fparams:tt $arrow:tt $rtrn_type:ty $fbody:block)) => $();
 }
+
+// CURRENT ERROR : UNABLE TO OVERLOAD FUNCTION (ALTHOUGH THERE ARE WAYS TO GET AROUND IT), CAN'T HAVE AMBIGUOUS RETURN STATEMENT
 
  recur_fn!{
     add_together (_num1 : u32, _num2 : u32) -> u32 {
@@ -28,5 +29,5 @@ macro_rules! recur_fn {
 
 fn main() {
    println!("Tail recursive macro almost done!!!");
-   println!("{:?}", add_together(0, 0));
+   //println!("{:?}", add_together(0, 0));
 }
